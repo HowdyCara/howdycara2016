2c047e2fe5d88411ec1841bff28547c85ea825e4
Cara Smith <howdycara@gmail.com>
2016-03-29 17:21:13 -0400

    Fix bug on portfolio feeds, add portfolio items

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,31 +1,113 @@
 var portfolioItems = [
   {
-      "image":"http://placehold.it/400x300",
-      "image-alt":"SHIFT logo 2016 Cara Smith howdycara.com",
-      "title":"SHIFT logo",
-      "client":"SHIFT",
+      "image":"img/800x600-electropolis-01.jpg",
+      "image-alt":"Electropolis art and logo design | Cara Smith",
+      "title":"Logo and art development",
+      "client":"Game theme: maximum capacity",
       "date":"2016",
-      "description":"Logo created for SHIFT, a company that invests in social causes.",
-      "type":"logo"
-  },
-  {
-      "image":"http://placehold.it/400x300",
-      "image-alt":"SHIFT logo 2016 Cara Smith howdycara.com",
-      "title":"Game",
-      "client":"SHIFT",
-      "date":"2016",
-      "description":"Logo created for SHIFT, a company that invests in social causes.",
+      "description":"Art asset development and logo design for Electropolis for Train Jam 2016 - a 52 hour jam on a train from Chicago to San Francisco - a match-3 game about harnessing the power of lightning to power a city.",
       "type":"game"
   },
   {
-      "image":"http://placehold.it/400x300",
-      "image-alt":"blha hahahhas",
-      "title":"SHIFT",
-      "client":"SHIFT",
+      "image":"img/800x600-wzl-01.jpg",
+      "image-alt":"Warp Zone Louisville logo design and branding | Cara Smith",
+      "title":"Logo & branding",
+      "client":"Louisville Makes Games!",
       "date":"2016",
-      "description":"samwaisisge gamji",
+      "description":"Warp Zone Louisville logo design and branding.",
       "type":"logo"
+  },
+  {
+      "image":"img/800x600-cj-01.jpg",
+      "image-alt":"Cluster junk art and logo design | Cara Smith",
+      "title":"Logo and art development",
+      "client":"Game theme: growing",
+      "date":"2015",
+      "description":"Art asset development and logo design for Cluster Junk for Ludum Dare 34, a game about collecting trash to become the largest trash island in the Pacific.",
+      "type":"game"
+  },
+  {
+      "image":"img/800x600-adelante-01.jpg",
+      "image-alt":"Adelante Hispanic Achievers logo design and rebrand | Cara Smith",
+      "title":"Logo & rebrand",
+      "client":"Adelante Hispanic Achievers",
+      "date":"2016",
+      "description":"Adelante Hispanic Achievers logo design and rebrand.",
+      "type":"logo"
+  },
+  {
+      "image":"img/800x600-lmg-01.jpg",
+      "image-alt":"Louisville Makes Games! collateral design | Cara Smith",
+      "title":"Louisville Makes Games! flyer",
+      "client":"Louisville Makes Games!",
+      "date":"2015",
+      "description":"Flyer designed to serve as information for the newly-formed, non-profit organization.",
+      "type":"collateral"
+  },
+  {
+        "image":"img/800x600-urbanHoney-01.jpg",
+        "image-alt":"Gordon's Urban Honey concept art and logo for 6-hour game jam | Cara Smith",
+        "title":"Logo and concept art",
+        "client":"Game theme: bees",
+        "date":"2015",
+        "description":"Gordon's Urban Honey - Ironically hipster game concept art and logo for 6-hour game jam.",
+        "type":"game"
+    },
+  {
+      "image":"img/800x600-lub-03.jpg",
+      "image-alt":"LubriSynHA collateral design and rebrand | Cara Smith",
+      "title":"LubriSynHA Flyers",
+      "client":"LubriSynHA",
+      "date":"2013",
+      "description":"Flyer design and rebrand example of LubriSynHA",
+      "type":"collateral"
+  },
+  {
+      "image":"img/800x600-lub-01.jpg",
+      "image-alt":"Gatefold brochure rebranded design for LubriSynHA - Pet | Cara Smith",
+      "title":"Rebranded gatefold brochure – Pet",
+      "client":"LubriSynHA",
+      "date":"2013",
+      "description":"Gatefold brochure rebranded design for pet products for LubriSynHA",
+      "type":"design"
+  },
+  {
+      "image":"img/800x600-lub-04.jpg",
+      "image-alt":"Illustration and publication cover design for LubriSynLVS and Max-Strum | Cara Smith",
+      "title":"Illustration and design",
+      "client":"LubriSynLVS and Max-Strum",
+      "date":"2013",
+      "description":"Illustration and publication cover design for LubriSynLVS and Max-Strum",
+      "type":"design"
+  },
+  {
+      "image":"img/800x600-lub-02.jpg",
+      "image-alt":"Gatefold rebrand design for LubriSynHA - Equine| Cara Smith",
+      "title":"Rebranded gatefold brochure - Equine",
+      "client":"LubriSynHA",
+      "date":"2013",
+      "description":"Gatefold brochure rebranded design for equine products for LubriSynHA",
+      "type":"design"
+  },
+  {
+      "image":"img/800x600-lub-05.jpg",
+      "image-alt":"Label design for a•ha! lip balm | Cara Smith",
+      "title":"Product label design",
+      "client":"LubriSynHA",
+      "date":"2013",
+      "description":"Gatefold brochure design for equine products for LubriSynHA",
+      "type":"design"
+  },
+  {
+      "image":"img/800x600-lub-06.jpg",
+      "image-alt":"Rebranded label design for LubriSynHA | Cara Smith",
+      "title":"Rebranded product label design",
+      "client":"LubriSynHA",
+      "date":"2012",
+      "description":"Rebranded product label design for LubriSynHA",
+      "type":"design"
   }
+
 
 ];
 
